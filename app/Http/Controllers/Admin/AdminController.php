16666af4ec7b96a8515c90960e8c0e5d5d4d152d
Mohammad Imran <92107444+imranshf4@users.noreply.github.com>
2025-12-05 13:13:01 +0600

    Ai Content Generation Excise_File,generate content table add

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function adminLogout(Request $request) {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,101 @@
+<div class="sidenav-menu">
+
+    <!-- Brand Logo -->
+    <a href="index.html" class="logo">
+        <span class="logo-light">
+            <span class="logo-lg"><img src="{{ asset('backend/assets/images/logo.png')}}" alt="logo"></span>
+            <span class="logo-sm"><img src="{{ asset('backend/assets/images/logo-sm.png')}}" alt="small logo"></span>
+        </span>
+
+        <span class="logo-dark">
+            <span class="logo-lg"><img src="{{ asset('backend/assets/images/logo-dark.png')}}" alt="dark logo"></span>
+            <span class="logo-sm"><img src="{{ asset('backend/assets/images/logo-sm.png')}}" alt="small logo"></span>
+        </span>
+    </a>
+
+    <!-- Sidebar Hover Menu Toggle Button -->
+    <button class="button-sm-hover">
+        <i class="ri-circle-line align-middle"></i>
+    </button>
+
+    <!-- Full Sidebar Menu Close Button -->
+    <button class="button-close-fullsidebar">
+        <i class="ti ti-x align-middle"></i>
+    </button>
+
+    <div data-simplebar>
+
+        <!--- Sidenav Menu -->
+        <ul class="side-nav">
+            <li class="side-nav-title">
+                Menu
+            </li>
+
+            <li class="side-nav-item">
+                <a href="index.html" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-dashboard"></i></span>
+                    <span class="menu-text"> Dashboard </span>
+                    <span class="badge bg-danger rounded-pill">9+</span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="apps-chat.html" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-message"></i></span>
+                    <span class="menu-text"> Chat </span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="apps-calendar.html" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-calendar"></i></span>
+                    <span class="menu-text"> Calendar </span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="apps-email.html" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-mailbox"></i></span>
+                    <span class="menu-text"> Email </span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarInvoice" aria-expanded="false" aria-controls="sidebarInvoice" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-invoice"></i></span>
+                    <span class="menu-text"> Invoice</span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarInvoice">
+                    <ul class="sub-menu">
+                        <li class="side-nav-item">
+                            <a href="apps-invoices.html" class="side-nav-link">
+                                <span class="menu-text">Invoices</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
+                            <a href="apps-invoice-details.html" class="side-nav-link">
+                                <span class="menu-text">View Invoice</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
+                            <a href="apps-invoice-create.html" class="side-nav-link">
+                                <span class="menu-text">Create Invoice</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+        </ul>
+
+        <!-- Help Box -->
+        <div class="help-box text-center">
+            <h5 class="fw-semibold fs-16">Unlimited Access</h5>
+            <p class="mb-3 text-muted">Upgrade to plan to get access to unlimited reports</p>
+            <a href="javascript: void(0);" class="btn btn-danger btn-sm">Upgrade</a>
+        </div>
+
+        <div class="clearfix"></div>
+    </div>
+</div>
